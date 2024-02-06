c6f7c33a406ab7d7ed3636c7b5f0750f9630ebbb
imide <43657624+imide@users.noreply.github.com>
2024-02-06 15:43:50 -0700

    f

--- commands/team/roster.go
+++ commands/team/roster.go
@@ -2,7 +2,7 @@ package team
 
 import (
 	"fmt"
-	"log"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/imide/aalm/commands/cmdutil"
@@ -26,72 +26,61 @@ var Roster = cmdutil.Commands{
 }
 
 func rosterHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// Retrieve the team data
 	teamData, err := db.GetTeamData(i.ApplicationCommandData().Options[0].StringValue())
 	if err != nil {
-		log.Println("Error retrieving team data,", err)
-		cmdutil.SendInteractionResponse(s, i, cmdutil.CreateEmbed("⚠️ | **Warning**", "An error occurred while retrieving the team data.", 0xffcc4d))
+		cmdutil.SendInteractionResponse(s, i, cmdutil.CreateEmbed("⚠️ | **Warning**", fmt.Sprintf("An error occurred while retrieving the team data: %v", err), 0xffcc4d))
 		return
 	}
+
 	teamRole, err := s.State.Role(i.GuildID, teamData.RoleID)
 	if err != nil {
-		log.Println("Error retrieving team role,", err)
-		cmdutil.SendInteractionResponse(s, i, cmdutil.CreateEmbed("⚠️ | **Warning**", "An error occurred while retrieving the team role.", 0xffcc4d))
+		cmdutil.SendInteractionResponse(s, i, cmdutil.CreateEmbed("⚠️ | **Warning**", fmt.Sprintf("An error occurred while retrieving the team role: %v", err), 0xffcc4d))
 		return
 	}
 
-	// Create a new MessageEmbed
-	embed := &discordgo.MessageEmbed{
-		Title:       "Team Roster",
-		Description: fmt.Sprintf("Roster for %s", teamData.Name),
-		Color:       teamRole.Color, // Set the color to the role color
-		Thumbnail: &discordgo.MessageEmbedThumbnail{
-			URL: teamData.Logo, // Set the thumbnail to the team's logo
-		},
-	}
-
-	// Add a field for each player
-	var totalStars float32 = 0
+	var totalStars float32
+	fields := make([]*discordgo.MessageEmbedField, 0, len(teamData.Players)+3)
 	for _, playerInfo := range teamData.Players {
 		totalStars += playerInfo.Stars
-		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+		fields = append(fields, &discordgo.MessageEmbedField{
 			Name:   fmt.Sprintf("<@!%s>", playerInfo.ID),
 			Value:  fmt.Sprintf("Stars: %f", playerInfo.Stars),
 			Inline: true,
 		})
 	}
 
-	// Add a field for the coaches
-	var coaches string
-	for _, coachID := range teamData.Coaches {
-		coaches += fmt.Sprintf("<@!%s>, ", coachID)
-	}
-
-	embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+	coaches := strings.Join(teamData.Coaches, ", ")
+	fields = append(fields, &discordgo.MessageEmbedField{
 		Name:   "Coaches",
 		Value:  coaches,
 		Inline: true,
 	})
 
-	// Add a field for the franchise owner
 	ownerData, err := db.GetSpecificPlayerData(teamData.Owner, bson.M{"stars": 1, "_id": 1})
 	if err != nil {
-		log.Println("Error retrieving player data,", err)
-		cmdutil.SendInteractionResponse(s, i, cmdutil.CreateEmbed("⚠️ | **Warning**", "An error occurred while retrieving the player data.", 0xffcc4d))
+		cmdutil.SendInteractionResponse(s, i, cmdutil.CreateEmbed("⚠️ | **Warning**", fmt.Sprintf("An error occurred while retrieving the player data: %v", err), 0xffcc4d))
 		return
 	}
-	embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-		Name: "Franchise Owner",
-		Value: fmt.Sprintf(
-			"<@!%s>\nStars: %f", ownerData.ID, ownerData.Stars),
+
+	fields = append(fields, &discordgo.MessageEmbedField{
+		Name:  "Franchise Owner",
+		Value: fmt.Sprintf("<@!%s>\nStars: %f", ownerData.ID, ownerData.Stars),
 	})
 
-	// Add a field for the star cap and the total stars
-	embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+	fields = append(fields, &discordgo.MessageEmbedField{
 		Name:  "Stars",
 		Value: fmt.Sprintf("Star Cap: %f, Total Stars: %f", teamData.MaxStars, totalStars),
 	})
 
-	// Send the embed as a response to the interaction
+	embed := &discordgo.MessageEmbed{
+		Title:       "Team Roster",
+		Description: fmt.Sprintf("Roster for %s", teamData.Name),
+		Color:       teamRole.Color,
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+			URL: teamData.Logo,
+		},
+		Fields: fields,
+	}
+
 	cmdutil.SendInteractionResponse(s, i, embed)
 }
